9b866d6084f72edb1664057d20c5127d026d376d
anderson <andersonbc97@gmail.com>
2025-11-03 21:27:46 -0300

    projeto criado

---
+++ src/main/java/com/backdevanderson/studiojoice/StudiojoiceApplication.java
@@ -0,0 +1,13 @@
+package com.backdevanderson.studiojoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiojoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiojoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backdevanderson/studiojoice/StudiojoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backdevanderson.studiojoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiojoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
